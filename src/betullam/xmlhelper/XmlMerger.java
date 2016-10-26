1323569a8b023d7e11044326316ac8d3c6fdd3b1
betullam <betullam@yahoo.de>
2016-10-26 11:06:09 +0200

    Finalized new XML merger.

--- src/betullam/xmlhelper/XmlMerger.java
+++ src/betullam/xmlhelper/XmlMerger.java
@@ -8,25 +8,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.apache.commons.lang3.StringEscapeUtils;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.Attributes;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.InputSource;
@@ -36,191 +20,14 @@ import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
 
 public class XmlMerger {
+	
 
-	Document document;
-
-	/**
-	 * Gets the last node with the specified tag name.
-	 * @param	tagName		a String with the tag name
-	 * @return				a Node object or null
-	 */
-	public Node getLastElementNode(String tagName) {
-		Node returnNode = null;
-		NodeList recordNodes = document.getElementsByTagName(tagName);		
-		returnNode = recordNodes.item(recordNodes.getLength()-1);
-		return returnNode;
-	}
-
-
-	/**
-	 * Gets the node with the specified tag name. If there are nested nodes with the same tag name,
-	 * use nodeCount to specify which node to take (starting with 0). If only one node exists with the
-	 * specified tag name, set nodeCount to 0. ATTENTION: This will only merge if the XML file contains
-	 * only 1 element!
-	 * @param tagName		a String with the tag name.
-	 * @param nodeCount 	an Integer with the number of the node
-	 * @return
-	 */
-	public Node getElementNode(String tagName, int nodeCount) {
-		Node returnNode = null;
-		NodeList recordNodes = document.getElementsByTagName(tagName);		
-		returnNode = recordNodes.item(nodeCount);
-		return returnNode;
-	}
-
-
-	/**
-	 * IMPORTANT: Works only wiht XML-files having only one element!
-	 * Checks a source directory for multiple XML files that should be merged togehter into one XML file. You may specify which node to copy from
-	 * the old XML files to the new one that will be the result of the merging process. If there are multiple nested nodes with the same name, you
-	 * may specify which one to take by the nodeCount parameter (if there is only one, use "0"). You also need to specify the name of the parent node,
-	 * in which the copied nodes will be nested.
-	 * 
-	 * @param sourceDirectory	a String that specifies the full path to a directory with multiple XML files that should be merged.
-	 * @param destinationFile	a String that specifies the full path and name of a XML file that is the result of the merging process (Warning: it cannot be in the same path as the source files!)
-	 * @param parentNode		a String that specifies the parent node in which the merged nodes will be nested.
-	 * @param nodeToMerge		a String that specifies the name of the nodes that should be copied from the old file to the new one.
-	 * @param nodeCount			an Integer: Use 0 as default. If your source XML-files have nested nodes with the same name, use this to specify which node to take.
-	 * @return 					true if the merge was successful, false otherwise
-	 */
-	public boolean mergeElementNodes(String sourceDirectory, String destinationFile, String parentNode, String nodeToMerge, int nodeCount) {
-		boolean isMergingSuccessful = false;
-		//System.out.println("Started merging element nodes, please wait ...");
-		File fSourceDirectory = new File(sourceDirectory);
-		File fDestinationFile = new File(destinationFile);
-		if (fSourceDirectory.getAbsolutePath().equals(fDestinationFile.getParent())) {
-			System.err.println("WARNING: Stopped merging process.\nIt's not possible to save the destination file in the source directory. Please specify another path for your destination file!");
-			return isMergingSuccessful;
-		}
-
-		if (!fSourceDirectory.exists()) {
-			System.err.println("WARNING: Stopped merging process.\nDirectory with multiple xml files does not exist!");
-			return isMergingSuccessful;
-		}
-
-		try {
-			OutputStream out = new BufferedOutputStream(new FileOutputStream(fDestinationFile.getAbsolutePath()));
-			PrintWriter writer = new PrintWriter(out);
-
-			//int counter = 0;
-			writer.println("<" + parentNode + ">");
-			File[] files = fSourceDirectory.listFiles();
-			Arrays.sort(files);
-			for (File xmlFile : files) {
-
-				this.setDocument(xmlFile.getAbsolutePath());
-				Node elementNode = this.getElementNode(nodeToMerge, nodeCount);
-				writer.println(nodeToString(elementNode));
-
-				/*
-				List<Node> elementNodeList = this.getElementNode(nodeToMerge, nodeCount);
-				for (Node elementNode : elementNodeList) {
-					counter = counter + 1;
-					writer.println(nodeToString(elementNode));
-				}
-				 */
-			}
-			writer.println("</" + parentNode + ">");
-
-			if (writer!=null) { writer.close(); }
-			isMergingSuccessful = true;
-			//System.out.println("Merging done!");
-		} catch (FileNotFoundException e) {
-			isMergingSuccessful = false;
-			e.printStackTrace();
-		}
-
-		return isMergingSuccessful;
-
-	}
-
-
-
-
-
-	// TODO: Merge ALL nodes of an XML file (see example code below), not only the first one that is found.
-	// But it could be a problem with nested nodes (nodeCount).
-	// MERGES MULTIPLE NODES PER XML FILE - PROBLEM WITH nodeCount!
-	public List<Node> getMultipleElementNode(String tagName) {
-		List<Node> returnNodeList = new ArrayList<Node>();
-		NodeList recordNodes = document.getElementsByTagName(tagName);
-
-		int noOfElements = recordNodes.getLength();
-
-		for (int i = 0; i < noOfElements; i++) {
-			Node currentNode = recordNodes.item(i);
-			returnNodeList.add(currentNode);
-
-		}
-
-		return returnNodeList;
-	}
-
-	/**
-	 * IMPORTANT: Works with multiple nodes per XML file, but if there are nested nodes of the same name, you can not specify which to take!
-	 * Checks a source directory for multiple XML files that should be merged togehter into one XML file.
-	 * 
-	 * @param sourceDirectory	a String that specifies the full path to a directory with multiple XML files that should be merged.
-	 * @param destinationFile	a String that specifies the full path and name of a XML file that is the result of the merging process (Warning: it cannot be in the same path as the source files!)
-	 * @param parentNode		a String that specifies the parent node in which the merged nodes will be nested.
-	 * @param nodeToMerge		a String that specifies the name of the nodes that should be copied from the old file to the new one.
-	 * @return 					true if the merge was successful, false otherwise
-	 */
-	public boolean mergeMultipleElementNodes(String sourceDirectory, String destinationFile, String parentNode, String nodeToMerge) {
-		boolean isMergingSuccessful = false;
-		//System.out.println("Started merging element nodes, please wait ...");
-		File fSourceDirectory = new File(sourceDirectory);
-		File fDestinationFile = new File(destinationFile);
-		if (fSourceDirectory.getAbsolutePath().equals(fDestinationFile.getParent())) {
-			System.err.println("WARNING: Stopped merging process.\nIt's not possible to save the destination file in the source directory. Please specify another path for your destination file!");
-			return isMergingSuccessful;
-		}
-
-		if (!fSourceDirectory.exists()) {
-			System.err.println("WARNING: Stopped merging process.\nDirectory with multiple xml files does not exist!");
-			return isMergingSuccessful;
-		}
-
-		try {
-			OutputStream out = new BufferedOutputStream(new FileOutputStream(fDestinationFile.getAbsolutePath()));
-			PrintWriter writer = new PrintWriter(out);
-
-			//int counter = 0;
-			writer.println("<" + parentNode + ">");
-			File[] files = fSourceDirectory.listFiles();
-			Arrays.sort(files);
-			for (File xmlFile : files) {
-
-				this.setDocument(xmlFile.getAbsolutePath());
-				List<Node> elementNodeList = this.getMultipleElementNode(nodeToMerge);
-				for (Node elementNode : elementNodeList) {
-					writer.println(nodeToString(elementNode));
-				}
-
-			}
-			writer.println("</" + parentNode + ">");
-
-			if (writer!=null) { writer.close(); }
-			isMergingSuccessful = true;
-			//System.out.println("Merging done!");
-		} catch (FileNotFoundException e) {
-			isMergingSuccessful = false;
-			e.printStackTrace();
-		}
-
-		return isMergingSuccessful;
-
-	}
-
-
-	// TODO: Merge elements using a SAX Parser
 	public boolean mergeElements(String sourceDirectory, String destinationFile, String parentElement, String elementToMerge, int elementLevel) {
 		boolean isMergingSuccessful = false;
 
-		//File fSourceDirectory = new File(sourceDirectory);
-		//File fSourceDirectory = new File("/home/betullam/AK/AKsearch/OaiData/goobi/original/1476893416113");
-		File fSourceDirectory = new File("/home/betullam/AK/AKsearch/OaiData/goobi/original/test_multiple_records");
+		File fSourceDirectory = new File(sourceDirectory);
 		File fDestinationFile = new File(destinationFile);
+		
 		if (fSourceDirectory.getAbsolutePath().equals(fDestinationFile.getParent())) {
 			System.err.println("WARNING: Stopped merging process.\nIt's not possible to save the destination file in the source directory. Please specify another path for your destination file!");
 			return isMergingSuccessful;
@@ -243,28 +50,35 @@ public class XmlMerger {
 			PrintWriter writer = new PrintWriter(out);
 
 			// Set ContentHandler:
-			XmlContentHandler xmlContentHandler = new XmlContentHandler(parentElement, elementToMerge, elementLevel, writer);
+			XmlContentHandler xmlContentHandler = new XmlContentHandler(elementToMerge, elementLevel, writer);
 			xmlReader.setContentHandler(xmlContentHandler);
 
-			//int counter = 0;
+			// Add XML intro tag
 			writer.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			
+			// Open with given parent element
 			writer.println("<" + parentElement + ">");
+			
+			// Sort files for proper iteration
 			File[] files = fSourceDirectory.listFiles();
 			Arrays.sort(files);
-
+			
+			// Iterate over files that should be merged
 			for (File xmlFile : files) {
-				// Specify XML-file to parse.
+				// Specify XML-file to parse
 				FileReader reader = new FileReader(xmlFile);
 				InputSource inputSource = new InputSource(reader);
 
-				System.out.println("\n\nStart parsing ...");
-
-				// Start parsing & indexing:
+				// Start parsing
 				xmlReader.parse(inputSource);				
 			}
+			
+			// Close given parent element
 			writer.println("</" + parentElement + ">");
 
+			// Close the writer
 			if (writer!=null) { writer.close(); }
+			
 			isMergingSuccessful = true;
 
 		} catch (FileNotFoundException e) {
@@ -276,71 +90,36 @@ public class XmlMerger {
 		}
 
 		return isMergingSuccessful;
-
-
 	}
 
-	public void setDocument(String xmlFile) {
-		this.document = getXmlDocument(new File(xmlFile));
-	}
-
-
-	public Document getDocument() {
-		return document;
-	}
-
-
-	private String nodeToString(Node node) {
-		String xmlString = null;
-		try {
-			StringWriter stringWriter = new StringWriter();
-			Transformer transformer = TransformerFactory.newInstance().newTransformer();
-			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-			transformer.transform(new DOMSource(node), new StreamResult(stringWriter));
-			xmlString = stringWriter.toString();
-		} catch (TransformerException e) {
-			e.printStackTrace();
-		}
-		return xmlString;
-	}
-
-
-
-	private Document getXmlDocument(File xmlFile) {
-		DocumentBuilderFactory dbFactory;
-		DocumentBuilder dBuilder;
-		Document doc = null;
-		try {
-			dbFactory = DocumentBuilderFactory.newInstance();
-			dBuilder = dbFactory.newDocumentBuilder();
-			doc = dBuilder.parse(xmlFile);
-			doc.getDocumentElement().normalize();
-		} catch (SAXException | IOException | ParserConfigurationException e) {
-			e.printStackTrace();
-		}
-
-		return doc;
-	}
-
+	
+	/**
+	 * Content Handler for merging XML elements.
+	 */
 	private class XmlContentHandler implements ContentHandler {
 
-		String parentElement;
 		String elementToMerge;
 		PrintWriter writer;
 		String elementContent;
-		String fullXmlString;
+		String fullXmlString = "";
 		int elementLevel = 0;
 		int elementLevelCounter = 0;
 		boolean withinElement = false;
-		boolean withinLevel = false;
 
-		private XmlContentHandler(String parentElement, String elementToMerge, int elementLevel, PrintWriter writer) {
+		/**
+		 * Constructor for the Content Handler that helps merging XML elements.
+		 * 
+		 * @param elementToMerge	String: The XML element that should be merged, e. g. "record"
+		 * @param elementLevel		int: The level of the element if there are nested elements of the same name. For the top-level element, use 1.
+		 * @param writer			PrintWriter: A writer that is responsible for writing the appropriate elements to a file.
+		 */
+		private XmlContentHandler(String elementToMerge, int elementLevel, PrintWriter writer) {
 			this.elementToMerge = elementToMerge;
-			this.parentElement = parentElement;
 			this.elementLevel = elementLevel;
 			this.writer = writer;
 		}
 
+		
 		/**
 		 * Encounters start of element.<br><br>
 		 * {@inheritDoc}
@@ -352,26 +131,24 @@ public class XmlMerger {
 
 
 			if(localName.equals(elementToMerge)) {
-				withinElement = true;
+				// We encounter the given element, so we count it's level
 				elementLevelCounter = elementLevelCounter + 1;
-				
-				// If we encounter the given element at the appropriate level, reset the String for the full XML for a fresh start
+								
+				// We encounter the given element at the appropriate level
 				if (elementLevelCounter == elementLevel) {
-					fullXmlString = "";
-				}
-				
-				if (elementLevelCounter == elementLevel) {
-					withinLevel = true;
-				}				
+					
+					// Set a variable that tells us that we are within the appropriate element
+					withinElement = true;
+				}			
 			}
 
 
-
-			if (withinElement && withinLevel) {
-
-				System.out.println("<" + qName + ">");
+			if (withinElement) {
 				
+				// Open the XML-start-tag and add it to a String variable
 				fullXmlString += "<" + qName;
+				
+				// Loop over any XML attribute and add it the XML-start-tag
 				for (int a = 0; a < atts.getLength(); a++) {
 					String attQName = atts.getQName(a);
 					String attValue = StringEscapeUtils.escapeXml10(atts.getValue(a));
@@ -381,8 +158,9 @@ public class XmlMerger {
 						fullXmlString += " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"";
 					}
 					fullXmlString += " " + attQName + "=\"" + attValue + "\"";
-
 				}
+				
+				// Close the XML-start-tag and add it to a String variable
 				fullXmlString += ">";
 			}
 		}
@@ -395,32 +173,43 @@ public class XmlMerger {
 		@Override
 		public void endElement(String uri, String localName, String qName) throws SAXException {
 
+			// Get the text content of the XML element
 			String content = elementContent.toString();
 			
-			if (withinElement && withinLevel) {
-				System.out.println("</" + qName + ">");
+			if (withinElement) {
+				// Add the escaped content to a String variable
 				fullXmlString += StringEscapeUtils.escapeXml10(content);
+				
+				// Reset the content variable for a fresh start
 				elementContent = "";
+				
+				// Add the XML-end-tag to a String variable
 				fullXmlString += "</" + qName + ">";
+				
 			}
 			
 			if(localName.equals(elementToMerge) ) {
-				withinElement = false;
-				if (elementLevelCounter < elementLevel) {
-					withinLevel = false;
-					System.out.println("End  : Within " + qName + " at level " + elementLevelCounter);
-				}
+				
+				// We encounter the end of the given element, so we decrease the level counter
 				elementLevelCounter = elementLevelCounter - 1;
-
-				writer.println(fullXmlString);
+				
+				if (elementLevelCounter < elementLevel) {
+					
+					// Set a variable that tells us that we are not within the appropriate element
+					withinElement = false;
+					
+					// Write the XML string to the file if it's not empty
+					if (!fullXmlString.trim().isEmpty()) {
+						writer.println(fullXmlString);
+					}
+					
+					// Reset the String for the full XML for a fresh start
+					fullXmlString = "";
+				}
 			}
-			
-			
-
-
-
-
 		}
+		
+
 
 		/**
 		 * Reads the content of the current XML element.<br><br>
@@ -431,11 +220,8 @@ public class XmlMerger {
 			elementContent += new String(ch, start, length);
 		}
 
-		public String getFullXmlString() {
-			return this.fullXmlString;
-		}
-
-
+		
+		
 
 		// Other methods that are not used at the moment
 		@Override
